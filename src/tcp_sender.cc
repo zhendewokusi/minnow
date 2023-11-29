19a32fa15b11c151e91c6f5c2aabc63db24a12bd
zhendewokusi <768672249@qq.com>
2023-11-29 16:35:14 +0800

    update lab3

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,9 +1,14 @@
 #include "tcp_sender.hh"
 #include "tcp_config.hh"
+#include "wrapping_integers.hh"
 
 #include <random>
 
 using namespace std;
+/*
+- keep track receiver's window
+- fill the window when possible
+*/
 
 /* TCPSender constructor (uses a random ISN if none given) */
 TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
@@ -12,38 +17,37 @@ TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
-  // Your code here.
-  return {};
+  return outstanding_cnt_;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
-  // Your code here.
-  return {};
+  return retransmission_cnt_;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
-  // Your code here.
   return {};
 }
 
 void TCPSender::push( Reader& outbound_stream )
 {
-  // Your code here.
-  (void)outbound_stream;
+  outbound_stream.bytes_buffered();
 }
 
+// 发送空消息
 TCPSenderMessage TCPSender::send_empty_message() const
 {
-  // Your code here.
-  return {};
+  auto seqno = Wrap32::wrap(next_seq_, Wrap32(isn_));
+  return {seqno,false,{},false};
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // Your code here.
-  (void)msg;
+  window_size_ = msg.window_size;
+  if(msg.ackno.has_value()){
+    ack_seq_ = msg.ackno.value().unwrap(Wrap32(isn_),next_seq_);
+  }
 }
 
 void TCPSender::tick( const size_t ms_since_last_tick )
